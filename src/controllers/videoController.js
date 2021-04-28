691565359004bab219a8e29f3b0ab24d1141d73d
unknown <junbox98221@gmail.com>
2021-04-28 15:20:12 +0900

    template done

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,34 @@
-import { response } from "express";
-
-export const trending = (req, res) => res.render("home");
+const videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+export const trending = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
